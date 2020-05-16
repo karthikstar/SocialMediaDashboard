2d83c4702aef795d0c1e0e8409e9f12fff530e10
karthikstar <64301417+karthikstar@users.noreply.github.com>
2020-05-16 17:25:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function darkmode(){
+
+$("body").toggleClass("darkmode")
+$(".headersection").toggleClass("darkmodeheader")
+$("h1,h2,h3,h4,p,label").toggleClass("darkmodetext")
+$("h5,h6").toggleClass("darkmodepurpletext")
+$(".followerstats,.overviewstats").toggleClass("darkmodecard")
+
+}
